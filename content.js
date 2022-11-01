42943302b766d38548221f47e65c78a16f2da51e
anwar sadatt <sadatt254@gmail.com>
2022-11-01 23:55:06 +0300

    added a fetch api

--- content.js
+++ content.js
@@ -1,20 +1,32 @@
 const exp = /^\$?-?0*(?:\d+(?!,)(?:\.\d{1,2})?|(?:\d{1,3}(?:,\d{3})*(?:\.\d{1,2})?))$/
 
-const subscription_key = '0aea31842bmsh241b9a498c15db8p1d300cjsn18352945a806'
-
-
-function converter(){
-
-}
-
-let elms = document.getElementsByTagName("*"),
-    len = elms.length;
-for(let ii = 0; ii < len; ii++) {
-    let myChildred = elms[ii].childNodes;
-    len2 = myChildred.length;
-    for (let jj = 0; jj < len2; jj++) {
-        if(myChildred[jj].nodeType === 3) {
-            myChildred[jj].nodeValue = myChildred[jj].nodeValue.replace(exp,"123");
-        }
+fetch('https://api.exchangerate.host/latest', {
+    method: 'GET',
+    headers: {
+        "Content-Type": 'application/json'
     }
-}
+})
+    .then(res => {
+        if(res.ok){
+            return res.json()
+        }else{
+            console.log('response not successfull')
+        }
+    })
+    .then(data => console.log(data.rates))
+    .catch(error => console.log(error))
+
+
+
+
+// let elms = document.getElementsByTagName("*"),
+//     len = elms.length;
+// for(let ii = 0; ii < len; ii++) {
+//     let myChildred = elms[ii].childNodes;
+//     len2 = myChildred.length;
+//     for (let jj = 0; jj < len2; jj++) {
+//         if(myChildred[jj].nodeType === 3) {
+//             myChildred[jj].nodeValue = myChildred[jj].nodeValue.replace(exp, convertCurrency(1000, 'EUR', 'USD'));
+//         }
+//     }
+// }
